865f217c499903c65f8c713e59f153a26c079aa2
Jatinpreet Singh <jatinpreet.singh@honeywell.com>
2015-10-02 23:26:02 +0530

    New method in a new branch

--- src/com/jatin/FirstGitClass.java
+++ src/com/jatin/FirstGitClass.java
@@ -5,4 +5,8 @@ public class FirstGitClass {
 		System.out.println("First Statement");
 		System.out.println("Second Statement");
 	}
+	
+	void printMethod(){
+		System.out.println("I am learning to work with branches in git");
+	}
 }
